051ea76c6bc9f36da87d06c114c0397575f6195f
LuisLoboR16 <lwolf95@hotmail.com>
2025-05-14 21:41:56 -0500

    Added Notification dashboard without design

---
+++ app/src/main/java/com/example/resiapp/AdminDashboardActivity.java
@@ -0,0 +1,60 @@
+package com.example.resiapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class AdminDashboardActivity extends AppCompatActivity {
+
+    LinearLayout layoutNotifications;
+    TextView txtNotifications;
+    ImageView imgNotifications;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_admin_dashboard);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        layoutNotifications = findViewById(R.id.layoutNotifications);
+        layoutNotifications.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(getApplicationContext(), NotificationActivity.class));
+            }
+        });
+
+
+        txtNotifications = findViewById(R.id.txtNotifications);
+        txtNotifications.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(getApplicationContext(), NotificationActivity.class));
+            }
+        });
+
+        imgNotifications = findViewById(R.id.imgNotifications);
+        imgNotifications.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(getApplicationContext(), NotificationActivity.class));
+            }
+        });
+    }
+        }
--- app/src/main/java/com/example/resiapp/LoginActivity.java
+++ app/src/main/java/com/example/resiapp/LoginActivity.java
@@ -30,7 +30,7 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                startActivity(new Intent(getApplicationContext(), ResidentDashboard.class));
+                startActivity(new Intent(getApplicationContext(), AdminDashboardActivity.class));
             }
         });
     }
