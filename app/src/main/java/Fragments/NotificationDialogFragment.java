757c284ee168fe271aa3c29d27e4bbe22f2407d6
LuisLoboR16 <lwolf95@hotmail.com>
2025-06-28 13:42:13 +0200

    Fixed send emails by resident and added send emails for all users by admin

--- app/src/main/java/Fragments/NotificationDialogFragment.java
+++ app/src/main/java/Fragments/NotificationDialogFragment.java
@@ -1,13 +1,17 @@
 package Fragments;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.app.Dialog;
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.app.AlertDialog;
 import android.widget.Spinner;
@@ -21,7 +25,6 @@ import androidx.fragment.app.DialogFragment;
 import com.android.volley.Request;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.example.resiapp.R;
-import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -33,13 +36,16 @@ import API.Constants;
 import API.SingleVolley;
 
 public class NotificationDialogFragment extends DialogFragment {
-    static String email = "";
+    static final String LOG_TAG = Constants.LOG_TAG;
     static final String adminEmail = Constants.ADMIN_EMAIL;
+    static final String SEND_EMAIL_ENDPOINT = Constants.SEND_EMAIL;
+    static String email = "";
 
     EditText edtEmail, edtSubject, edtComments;
     TextView txtSelectUser;
     Spinner spinnerUser;
-
+    CheckBox checkSendToAll;
+    List<String> allUserEmails = new ArrayList<>();
 
     @NonNull
     @Override
@@ -53,6 +59,7 @@ public class NotificationDialogFragment extends DialogFragment {
         edtComments = view.findViewById(R.id.editComment);
         txtSelectUser = view.findViewById(R.id.tvSelectUserNotification);
         spinnerUser = view.findViewById(R.id.editSpinnerUser);
+        checkSendToAll = view.findViewById(R.id.checkSendToAll);
 
         Button btnSend = view.findViewById(R.id.btnSend);
         Button btnCancel = view.findViewById(R.id.btnCancel);
@@ -62,6 +69,7 @@ public class NotificationDialogFragment extends DialogFragment {
             edtEmail.setEnabled(false);
             txtSelectUser.setVisibility(View.GONE);
             spinnerUser.setVisibility(View.GONE);
+            checkSendToAll.setVisibility(View.GONE);
 
             email = adminEmail;
 
@@ -69,27 +77,73 @@ public class NotificationDialogFragment extends DialogFragment {
             edtEmail.setVisibility(View.GONE);
             txtSelectUser.setVisibility(View.VISIBLE);
             spinnerUser.setVisibility(View.VISIBLE);
+            checkSendToAll.setVisibility(View.VISIBLE);
 
             loadUserEmails();
         }
 
         btnSend.setOnClickListener(v -> {
-            String subject = edtSubject.getText().toString();
-            String body = edtComments.getText().toString();
+            String subject = edtSubject.getText().toString().trim();
+            String body = edtComments.getText().toString().trim();
 
             if (!invokedClass.equals("ResidentDashboardActivity")) {
                 email = spinnerUser.getSelectedItem() != null ? spinnerUser.getSelectedItem().toString() : "";
             }
 
             if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(subject) && !TextUtils.isEmpty(body)) {
-                Intent intent = new Intent(Intent.ACTION_SEND);
-                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{email});
-                intent.putExtra(Intent.EXTRA_SUBJECT, subject);
-                intent.putExtra(Intent.EXTRA_TEXT, body);
-                intent.setType("message/rfc822");
-                startActivity(Intent.createChooser(intent, "Select a client for emails"));
-                dismiss();
+                try {
+                    JSONObject jsonBody = new JSONObject();
 
+                    JSONArray toArray = new JSONArray();
+                    toArray.put(email);
+
+                    String role = getUserRoleFromPrefs();
+                    String emailCC = getEmailFromPrefs();
+
+                    JSONArray ccArrayResident = new JSONArray();
+                    ccArrayResident.put(emailCC);
+
+                    if(role.equals("Resident")){
+                        jsonBody.put("to", toArray);
+                        jsonBody.put("subject", subject);
+                        jsonBody.put("body", body);
+                        jsonBody.put("role", role);
+                        jsonBody.put("cc", ccArrayResident);
+
+                    } else {
+
+                        if (checkSendToAll.isChecked() && !allUserEmails.isEmpty()) {
+                            for (String ccEmail : allUserEmails) {
+                                if (!ccEmail.equals(email)) {
+                                    toArray.put(ccEmail);
+                                }
+                            }
+                        }
+
+                        jsonBody.put("to", toArray);
+                        jsonBody.put("subject", subject);
+                        jsonBody.put("body", body);
+                        jsonBody.put("role", role);
+                    }
+
+                    Log.d(LOG_TAG, "Payload final: " + jsonBody);
+
+                    JsonObjectRequest request = new JsonObjectRequest(
+                            Request.Method.POST,
+                            Constants.URL + SEND_EMAIL_ENDPOINT,
+                            jsonBody,
+                            response -> {
+                                Toast.makeText(getContext(), "Email sent successfully", Toast.LENGTH_SHORT).show();
+                                dismiss();
+                            },
+                            error -> {
+                                Toast.makeText(getContext(), "Error sending email", Toast.LENGTH_LONG).show();
+                            }
+                    );
+                    SingleVolley.getInstance(requireContext()).getRequestQueue().add(request);
+                } catch (Exception e) {
+                    Toast.makeText(getContext(), "Unexpected error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                }
             } else {
                 Toast.makeText(getContext(), "Please fill all fields.", Toast.LENGTH_SHORT).show();
             }
@@ -118,6 +172,7 @@ public class NotificationDialogFragment extends DialogFragment {
                             JSONObject userJson = jsonArray.getJSONObject(i);
                             String userEmail = userJson.getString("email");
                             emailList.add(userEmail);
+                            allUserEmails.add(userEmail);
                         }
 
                         ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, emailList);
@@ -130,7 +185,17 @@ public class NotificationDialogFragment extends DialogFragment {
                 },
                 error -> Toast.makeText(getContext(), "Connection error", Toast.LENGTH_SHORT).show()
         );
-
         SingleVolley.getInstance(requireContext()).getRequestQueue().add(request);
+    }
+
+
+    private String getUserRoleFromPrefs() {
+        SharedPreferences prefs = requireContext().getSharedPreferences("user_prefs", MODE_PRIVATE);
+        return prefs.getString("role", "Resident");
+    }
+
+    private String getEmailFromPrefs() {
+        SharedPreferences prefs = requireContext().getSharedPreferences("user_prefs", MODE_PRIVATE);
+        return prefs.getString("resident_email","");
     }
 }
